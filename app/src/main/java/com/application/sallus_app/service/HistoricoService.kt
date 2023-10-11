c676928b653c82875d683fa1bc191167375dec9a
leonardotome <leonardo.eduardo@sptech.school>
2023-10-11 20:01:27 -0300

    HistoricoService criada.

--- app/src/main/java/com/application/sallus_app/service/HistoricoService.kt
+++ app/src/main/java/com/application/sallus_app/service/HistoricoService.kt
@@ -1,4 +1,11 @@
 package com.application.sallus_app.service
 
+import com.application.sallus_app.model.HistoricData
+import com.application.sallus_app.model.NutritionistData
+import retrofit2.http.GET
+
 interface HistoricoService {
+
+    @GET("diarios/historico")
+    suspend fun getHistorico(): List<HistoricData>
 }
