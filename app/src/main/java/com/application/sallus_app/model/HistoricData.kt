3f46572c8cb1c09a83a81e2ce4eae98939f209a7
leonardotome <leonardo.eduardo@sptech.school>
2023-10-11 20:43:10 -0300

    Ajuste na data class do histórico.

--- app/src/main/java/com/application/sallus_app/model/HistoricData.kt
+++ app/src/main/java/com/application/sallus_app/model/HistoricData.kt
@@ -4,11 +4,11 @@ import java.time.format.DateTimeFormatter
 import java.util.Date
 
 data class HistoricData(
-    val alimentos: String,
     val descricao: String,
     val qtdCalorias: Int,
     val periodo: String,
     val consumido: Boolean,
     val dataConsumir: Date,
-    val dataConsumida: DateTimeFormatter
+    val dataConsumida: DateTimeFormatter,
+    val alimentos: String
 )
