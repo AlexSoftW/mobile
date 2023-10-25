06c6b5d9c882e191e2e6f66acb5162a4d09b74aa
leonardotome <leonardo.eduardo@sptech.school>
2023-10-25 17:41:11 -0300

    Ajustes na fragment.

--- app/src/main/java/com/application/sallus_app/view/fragments/FragmentHomePaciente.kt
+++ app/src/main/java/com/application/sallus_app/view/fragments/FragmentHomePaciente.kt
@@ -1,9 +1,46 @@
 package com.application.sallus_app.view.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.application.sallus_app.R
 import com.application.sallus_app.databinding.FragmentItensHomePacienteBinding
+import com.application.sallus_app.databinding.FragmentTodosNutricionistaBinding
 
 class FragmentHomePaciente : Fragment() {
 
+
     private lateinit var binding: FragmentItensHomePacienteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentItensHomePacienteBinding.inflate(inflater, container, false)
+
+        binding.btnNutricionistas.setOnClickListener {
+//            val FragmentNutritionist = Intent(this, FragmentNutritionist::class.java)
+            val FragmentNutritionist = Intent(this, FragmentNutritionist::class.java)
+            startActivity(FragmentNutritionist)
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_paciente, fragment)
+                startActivity(FragmentNutritionist)
+                .commit()
+        }
+
+        return binding.root
+    }
+
 }
