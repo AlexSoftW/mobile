285488a8f69347dccc20c1563031c3bd1eb77fde
leonardotome <leonardo.eduardo@sptech.school>
2023-10-24 23:57:02 -0300

    Todos botões da badge_Paciente configurados.

--- app/src/main/java/com/application/sallus_app/view/HomePacienteActivity.kt
+++ app/src/main/java/com/application/sallus_app/view/HomePacienteActivity.kt
@@ -9,6 +9,7 @@ import com.application.sallus_app.R
 import com.application.sallus_app.databinding.ActivityLoginBinding
 import com.application.sallus_app.databinding.ActivityPacienteBinding
 import com.application.sallus_app.databinding.FragmentItensHomePacienteBinding
+import com.application.sallus_app.view.fragments.FragmentFoods
 import com.application.sallus_app.view.fragments.FragmentHomePaciente
 import com.application.sallus_app.view.fragments.FragmentNutritionist
 
@@ -42,17 +43,41 @@ class HomePacienteActivity : AppCompatActivity() {
         binding.includeBadgePaciente.imagebuttonHome.setOnClickListener {
             restoreOriginColor()
             binding.includeBadgePaciente.imagebuttonHome.setColorFilter(redColor)
+            replaceFragmentManager(FragmentHomePaciente())
+        }
+
+        binding.includeBadgePaciente.imagebuttonNutricionista.setOnClickListener {
+            restoreOriginColor()
+            binding.includeBadgePaciente.imagebuttonNutricionista.setColorFilter(redColor)
             replaceFragmentManager(FragmentNutritionist())
+        }
+
+        binding.includeBadgePaciente.imagebuttonFood.setOnClickListener {
+            restoreOriginColor()
+            binding.includeBadgePaciente.imagebuttonFood.setColorFilter(redColor)
+            replaceFragmentManager(FragmentFoods())
+        }
+
+        binding.includeBadgePaciente.imagebuttonDiarioAlimentar.setOnClickListener {
+            restoreOriginColor()
+            binding.includeBadgePaciente.imagebuttonDiarioAlimentar.setColorFilter(redColor)
+//            replaceFragmentManager(FragmentDiarioAlimentar())
+        }
+
+        binding.includeBadgePaciente.imagebuttonHistorico.setOnClickListener {
+            restoreOriginColor()
+            binding.includeBadgePaciente.imagebuttonHistorico.setColorFilter(redColor)
+//            replaceFragmentManager(FragmentHistorico())
         }
     }
 
     fun restoreOriginColor() {
         val originalColor = ContextCompat.getColor(this, R.color.black_100)
         binding.includeBadgePaciente.imagebuttonHome.setColorFilter(originalColor)
-        binding.includeBadgePaciente.imagebuttonHistorico.setColorFilter(originalColor)
+        binding.includeBadgePaciente.imagebuttonNutricionista.setColorFilter(originalColor)
         binding.includeBadgePaciente.imagebuttonFood.setColorFilter(originalColor)
         binding.includeBadgePaciente.imagebuttonDiarioAlimentar.setColorFilter(originalColor)
-        binding.includeBadgePaciente.imagebuttonNutricionista.setColorFilter(originalColor)
+        binding.includeBadgePaciente.imagebuttonHistorico.setColorFilter(originalColor)
     }
 
 
