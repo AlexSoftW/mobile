ee40032080f63fd8b5facfc22661011b3ab3cb18
leonardotome <leonardo.eduardo@sptech.school>
2023-10-24 23:25:12 -0300

    Criação da FragmentHomePaciente no Kotlin.

---
+++ app/src/main/java/com/application/sallus_app/view/fragments/FragmentHomePaciente.kt
@@ -0,0 +1,9 @@
+package com.application.sallus_app.view.fragments
+
+import androidx.fragment.app.Fragment
+import com.application.sallus_app.databinding.FragmentItensHomePacienteBinding
+
+class FragmentHomePaciente : Fragment() {
+
+    private lateinit var binding: FragmentItensHomePacienteBinding
+}
