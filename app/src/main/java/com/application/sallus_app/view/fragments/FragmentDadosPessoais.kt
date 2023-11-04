18df9a9486d693e47f08f627541881d4e4d61c41
leonardotome <leonardo.eduardo@sptech.school>
2023-11-04 16:44:28 -0300

    Método check inputs para verificação se as inputs estão preenchidas corretamente.

--- app/src/main/java/com/application/sallus_app/view/fragments/FragmentDadosPessoais.kt
+++ app/src/main/java/com/application/sallus_app/view/fragments/FragmentDadosPessoais.kt
@@ -3,6 +3,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.application.sallus_app.R
 import com.application.sallus_app.databinding.FragmentCadastroBinding
@@ -21,6 +22,9 @@ class FragmentDadosPessoais : Fragment() {
         val fragmentDestino = FragmentComorbidade()
 
         binding.proximo1.setOnClickListener {
+
+            checkInputs()
+
             val nome = binding.nomePaciente.text.toString()
             val telefone = binding.telefone.text.toString()
             val endereco = binding.endereco.text.toString()
@@ -45,4 +49,23 @@ class FragmentDadosPessoais : Fragment() {
         return binding.root
     }
 
+    private fun checkInputs() {
+        val nome = binding.nomePaciente.text.toString()
+        val telefone = binding.telefone.text.toString()
+        val endereco = binding.endereco.text.toString()
+        val genero = binding.genero.text.toString()
+
+            if (nome.isBlank()) {
+                binding.nomePaciente.error = "Preencha seu nome."
+            } else if (telefone.isBlank() || telefone.length < 9) {
+                binding.telefone.error = "Preencha seu telefone com 9 dígitos."
+            } else if (endereco.isBlank()) {
+                binding.endereco.error = "Preencha o seu endereço."
+            } else if (genero.isBlank()) {
+                binding.genero.error = "Selecione o seu gênero."
+            } else {
+                Toast.makeText(context, "Teste", Toast.LENGTH_SHORT).show()
+            }
+
+    }
 }
