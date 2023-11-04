3ba56a058821cb88d0329720297e3745879a2989
leonardotome <leonardo.eduardo@sptech.school>
2023-11-04 18:20:32 -0300

    Método checkInput no cadastro do email do nutricionista.

--- app/src/main/java/com/application/sallus_app/view/fragments/FragmentEmailNutri.kt
+++ app/src/main/java/com/application/sallus_app/view/fragments/FragmentEmailNutri.kt
@@ -4,9 +4,11 @@ package com.application.sallus_app.view.fragments
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.application.sallus_app.R
 import com.application.sallus_app.databinding.FragmentCadastroEmailBinding
@@ -44,10 +46,13 @@ class FragmentEmailNutri : Fragment() {
             )
 
             binding.nextButton1.setOnClickListener {
+
+                checkInputs()
+
                 val email = binding.email.text.toString()
                 val senha = binding.senha.text.toString()
-
                 val confirmrSenha = binding.confirmarSenha.text.toString()
+
                 bundle.putString("Email", email)
                 bundle.putString("Senha", senha)
                 bundle.putString("ConfirmarSenha", confirmrSenha)
@@ -88,5 +93,35 @@ class FragmentEmailNutri : Fragment() {
     fun retornarFragment() {
         val fragmentManager = requireActivity().supportFragmentManager
         fragmentManager.popBackStack()
+    }
+
+    private fun checkInputs() {
+        val email = binding.email.text.toString()
+        val password = binding.senha.text.toString()
+        val confirmPassword = binding.confirmarSenha.text.toString()
+
+        if (email.isBlank() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.email.error = "Preencha um email válido."
+        } else if (!isPasswordValid(password)) {
+            binding.senha.error = "Preencha uma senha válida."
+        } else if (password != confirmPassword) {
+            binding.confirmarSenha.error = "As senhas não coincidem."
+        } else {
+            Toast.makeText(context, "Teste", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        if (password.isBlank()) {
+            return false  // A senha está vazia, portanto, é inválida
+        }
+
+        val hasUppercase = password.any { it.isUpperCase() }
+        val hasLowercase = password.any { it.isLowerCase() }
+        val hasDigit = password.any { it.isDigit() }
+        val hasSpecialChar = password.any { it.isLetterOrDigit().not() }
+        val isLengthValid = password.length >= 8
+
+        return hasUppercase && hasLowercase && hasDigit && hasSpecialChar && isLengthValid
     }
 }
